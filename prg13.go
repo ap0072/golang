7ea580a44d0583f47fe8012f3a0c7c9528b4f43f
CHALUVADI PRASANTH KUMAR <chaluvadikumar97@gmail.com>
2024-11-11 22:12:29 +0530

    Adding the bitwise operator

--- prg13.go
+++ prg13.go
@@ -6,15 +6,16 @@ import "fmt"
 func main() {
 	var a int = 10
 	var b int = 2
-	fmt.Printf("the boolean value of %d > %d , is %t \n", a, b, a > b)
-	fmt.Printf("the boolean value of %d < %d , is %t \n", a, b, a < b)
-	//using Logical and operator - &&
-	c := (a > b) && (a < b)
-	fmt.Printf("the boolean value of (%d > %d) and (%d < %d) , is %t \n", a, b, a, b, c)
-	//using Logical or operator - ||
-	d := (a > b) || (a < b)
-	fmt.Printf("the boolean value of (%d > %d) or (%d < %d) , is %t \n", a, b, a, b, d)
-	//using Logical not operator - !
-	fmt.Printf("the boolean value of not of false , is %t \n", !c)
-	fmt.Printf("the boolean value of not of true , is %t \n", !d)
+
+	//using Bitwise and operator - &
+	c := a & b
+	fmt.Printf("the bitwise AND operator value of %d ,  %d is %d \n", a, b, c)
+
+	//using Bitwise or operator - |
+	c := a | b
+	fmt.Printf("the bitwise OR operator value of %d ,  %d is %d \n", a, b, c)
+
+	//using Bitwise xor operator - |
+	c := a ^ b
+	fmt.Printf("the bitwise XOR operator value of %d ,  %d is %d \n", a, b, c)
 }
